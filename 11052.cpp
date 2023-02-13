dc43c48ec8f81556311d360071f86889ba35052c
chanwoong1 <jcw7468@gmail.com>
2023-02-13 19:56:31 +0900

    feat: 11052

--- 11052.cpp
+++ 11052.cpp
@@ -8,38 +8,24 @@
 
 using namespace std;
 
+int N;
+int Card[10001];
+int DP[10001];
+
+int _max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 int main () {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int N;
-    vector<int> card;
-    int c;
     cin >> N;
-    card.push_back(0);
-    for (int i = 0; i < N; ++i) {
-        cin >> c;
-        card.push_back(c);
-    }
-    long dp[10001];
-    dp[0] = 0;
-    long tmp = 0;
-    for (int i = 1; i <= N; ++i) {
-        dp[i] = card[i] * (N / i);
-        if (N % i != 0) {
-            long tmp2 = 0;
-            int idx = 0;
-            for (int j = 1; j < N % i; ++j) {
-                if (card[j] * ((N % i) / j) > tmp2) {
-                    tmp2 = card[j] * ((N % i) / j);
-                    idx = j;
-                }
-            }
-            dp[i] += tmp2;
-        }
-        if (dp[i] > tmp)
-            tmp = dp[i];
-        cout << dp[i] << " ";
-    }
-    cout << "\n" << tmp;
+    for (int i = 1; i <= N; ++i)
+        cin >> Card[i];
+    DP[0] = Card[0] = 0;
+    for (int i = 1; i <= N; ++i)
+        for (int j = 1; j <= i; ++j)
+            DP[i] = _max(DP[i], DP[i - j] + Card[j]);
+    cout << DP[N];
 }
