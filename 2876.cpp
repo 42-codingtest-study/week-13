7348cd52b8f5ae5c275d1a1b8339a551fb442d81
chanwoong1 <jcw7468@gmail.com>
2023-02-14 19:35:59 +0900

    feat: 5

---
+++ 2876.cpp
@@ -0,0 +1,23 @@
+// 그래픽스 퀴즈
+// https://www.acmicpc.net/problem/2876
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+    fast;
+    int N;
+    cin >> N;
+    int A[N], B[N];
+    for (int i = 0; i < N; ++i)
+        cin >> A[i] >> B[i];
+
+}
---
+++ 9655.cpp
@@ -0,0 +1,22 @@
+// 돌 게임
+// https://www.acmicpc.net/problem/9655
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin >> N;
+    (N % 2 == 0) ? cout << "CY" : cout << "SK";
+    return 0;
+}
