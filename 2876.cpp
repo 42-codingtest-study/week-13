d73b00f6de5581bc87451efc2c6e5046301c9f2f
chanwoong1 <jcw7468@gmail.com>
2023-02-18 21:26:01 +0900

    feat: 2876

--- 2876.cpp
+++ 2876.cpp
@@ -11,13 +11,32 @@ using namespace std;
 
 #define fast ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
 #define ll long long
-
+int dp[6] = {0, 0, 0, 0, 0, 0};
 int main() {
     fast;
     int N;
     cin >> N;
-    int A[N], B[N];
-    for (int i = 0; i < N; ++i)
-        cin >> A[i] >> B[i];
-
+    int a, b;
+    int m = 0, grade;
+    for (int i = 0; i < N; ++i) {
+        cin >> a >> b;
+        if (b < a) {
+            int tmp = b;
+            b = a;
+            a = tmp;
+        }
+        for (int j = 1; j < 6; ++j) {
+            if (j == a || j == b) dp[j]++;
+            else dp[j] = 0;
+        }
+        if (dp[a] > m) {
+            m = dp[a];
+            grade = a;
+        }
+        if (dp[b] > m) {
+            m = dp[b];
+            grade = b;
+        }
+    }
+    cout << m << " " << grade;
 }
