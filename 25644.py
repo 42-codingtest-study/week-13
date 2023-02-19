4c5e0a6ff9e759a486c0c41c4937317e51e8bda3
박예은 <yeeupark@bag-yeeun-ui-MacBookPro.local>
2023-02-19 22:37:11 +0900

    week_13

--- 25644.py
+++ 25644.py
@@ -8,7 +8,7 @@ A = list(map(int, input().split()))
 max = 0 #이익 최대값
 res = 0 #결과값
 
-for i in range(N-1, -1, -1) :
+for i in range(N - 1, -1, -1) :
     benefit = max(max, A[i])
     res = max(res, max - A[i])
 
